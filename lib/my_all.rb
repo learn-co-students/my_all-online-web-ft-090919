40992eba01f8d8a0c359df5ffd48eed4f4a08d8a
= <=>
2019-09-10 16:54:27 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_return_values = []
+  i = 0
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  !block_return_values.include?(false)
 end
